49bdd1f9f50d6b9a6182c1249fa63500dc723e16
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-15 14:23:22 +0200

    struttura

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var stampa = document.getElementById("stampa")
+
 function run() {
     var arrayStudente = {
         student: []
@@ -6,29 +8,52 @@ function run() {
     var name = document.getElementById("name").value
     var surname = document.getElementById("surname").value
     var age = parseInt(document.getElementById("age").value)
-    
-    // lo facciamo in un modo un po' "anonimo", per evitare dopo
-    // di sovrascrivere lo stesso oggetto.
-    arrayStudente.student.push({
+        arrayStudente.student.push({
         name: name,
         surname: surname,
         age: age
     });
     
     
-    for (var i = 0; i < arrayStudente.student.length; i++) {
-        console.log(arrayStudente.student[i])
-        for (var key in arrayStudente.student[i]) {
-            console.log(key , arrayStudente.student[i] [key])
-        }    
+    for (var key in arrayStudente) {
+        console.log(key, arrayStudente[key])
     }
     
-    return arrayStudente
+    var stampa = document.getElementById('stampa');
+    
+    // stampa.innerHTML = '<li>model: </li>'
+    var varMomentanea = '';
+    
+    for (var key in arrayStudente) {  // ciclo nell'oggetto
+    
+        if (key === 'student') { // qui scelgo l'array
+            varMomentanea += '<li>Studente: ';
+    
+            for (var x = 0; x < arrayStudente[key].length; x++) { // ciclo l'array dei student
+                varMomentanea += arrayStudente[key][x]['name'] + ', ';
+                varMomentanea += arrayStudente[key][x]['surname'] + ', ';
+                varMomentanea += arrayStudente[key][x]['age'] + ', ';
+            }
+    
+            varMomentanea += '</li>';
+    
+        } else {
+            varMomentanea += '<li>' + key + ': ' + arrayStudente[key] + '</li>'
+        }
+    }
+    
+    stampa.innerHTML += varMomentanea;
+    
+        
 }
+
+
     function svuota() {  
         document.getElementById("name").value = "";
         document.getElementById("surname").value = "";
         document.getElementById("age").value = "";
     
       }
+
+
     
